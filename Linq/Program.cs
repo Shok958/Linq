daf46d0d1e1adb997e1fd65c006391923e9445bb
Antonio <antonio958@hotmail.com>
2017-08-03 12:33:43 +0200

    meh

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -10,6 +10,32 @@ namespace Linq
     {
         static void Main(string[] args)
         {
+
+            var a = 10;
+            a.Between(2, 11);
+
+            var numbers = new List<int>()
+            {
+                1,2,3
+            };
+
+            numbers.Where(Predicate);
+
+            numbers.Where(n => n > 1);//hace lo mismo que arriba
+        }
+
+        static bool Predicate(int value) {
+            return value > 1;
         }
     }
+
+    public static class ExtensionInt {
+
+        public static bool Between(this int value, int from, int to) {
+            return value >= from && value <= to;
+        }
+
+    }
 }
+
+
